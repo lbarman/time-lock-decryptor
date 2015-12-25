2bfba8e6fbd236a4fceb9f2b7fc3ab84175f698a
Ludovic Barman <ludovic.barman@gmail.com>
2015-12-25 15:44:17 +0100

    Working version with trapdoor

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"flag"
 	"bufio"
 	"fmt"
 	"math/rand"
@@ -12,6 +13,8 @@ import (
 	"encoding/hex"
 	"io/ioutil"
 )
+
+var expOneSecond *big.Int
 
 func newPrime(pow int) *big.Int {
 	//we pick the first prime
@@ -55,20 +58,36 @@ func main() {
 	fmt.Println("Time-Lock Puzzle")
 	fmt.Println("****************")
 
-	/*
+	expOneSecond =ExpByPowOfTwo(big.NewInt(2), big.NewInt(5000))
+
+	gen := flag.Bool("gen", false, "Start generating a new puzzle")
+	solve := flag.Bool("solve", true, "Start solving a puzzle")
+
+	if *gen {
+		preparePuzzle()
+	} else if *solve {
+		solvePuzzle()
+	} else {
+		fmt.Println("Please add the flag -gen or -solve in the command line for the desired action.")
+	}
+
+	fmt.Println("****************")
+}
+
+func preparePuzzle() {
+
+	//randomness
+	rndSource := rand.NewSource(time.Now().UnixNano())
+	rnd       := rand.New(rndSource)
+
 	//we generate the parameters
 	p := newPrime(1234)
 	q := newPrime(1010)
+	one := big.NewInt(1)
+	p_1 := big.NewInt(0).Sub(p, one)
+	q_1 := big.NewInt(0).Sub(q, one)
+	phi := big.NewInt(0).Mul(p_1, q_1)
 	n := big.NewInt(0).Mul(p, q)
-
-	WriteBig(n, "n.dat")
-
-	fmt.Println("Parameters are :")
-	fmt.Println("N is", n)
-	fmt.Println("Its hash is", sha256AndHex(n.Bytes()))
-	*/
-
-	n := ReadBig("n.dat")
 
 	fmt.Println("Parameters are :")
 	fmt.Println("N is", n)
@@ -77,12 +96,8 @@ func main() {
 	//generate benchmark, a 1-second exponentiation
 	repeat := 10
 	times := make([]time.Duration, repeat)
-	expOneSecond :=ExpByPowOfTwo(big.NewInt(2), big.NewInt(5000))
 
 	for i:=0; i<repeat; i++ {
-
-		rndSource := rand.NewSource(time.Now().UnixNano())
-		rnd       := rand.New(rndSource)
 		rand      := big.NewInt(0).Rand(rnd, n)
 		start     := time.Now()
 
@@ -96,28 +111,20 @@ func main() {
 	fmt.Println("****************")
 	fmt.Println("All clear !")
 
-	reader := bufio.NewReader(os.Stdin)
-
 	proceed := false
 	x := big.NewInt(0)
 	y := big.NewInt(0)
 
 	for !proceed{
-		 fmt.Print("Enter base : ")
-	    base, _ := reader.ReadString('\n')
-	    base = strings.Replace(base, "\n", "", 1)
+		x = big.NewInt(0).Rand(rnd, n)
 
-	    fmt.Print("Enter number of iterations : ")
-	    iter, _ := reader.ReadString('\n')
-	    iter = strings.Replace(iter, "\n", "", 1)
+		y = readBig("Enter number of iterations : ")
 
-	    x.SetString(base, 10)
-	    y.SetString(iter, 10)
-
-	    fmt.Println("Base is", x, "Iter is", y)
+	    fmt.Println("Iter is", y)
 	    duration := time.Duration(int(y.Int64())) * time.Second
 	    fmt.Println("Expected duration is", duration)
-	    fmt.Print("Proceed [y/n]")
+	    fmt.Println("Proceed [y/n] ?")
+	    reader := bufio.NewReader(os.Stdin)
 	    ans, _ := reader.ReadString('\n')
 	    ans = strings.Replace(ans, "\n", "", 1)
 
@@ -126,11 +133,13 @@ func main() {
 	    }
 	}
 
-	curr := big.NewInt(0)
-	one := big.NewInt(1)
-	zero := big.NewInt(0)
+	curr    := big.NewInt(0)
+	zero    := big.NewInt(0)
 	hundred := big.NewInt(100)
+	base    := x
+	iter    := y
 
+	exponent := one
 	for curr.Cmp(y) < 0 {
 		if mod(curr, hundred).Cmp(zero) == 0 {
 			diff := big.NewInt(0).Sub(y, curr)
@@ -138,24 +147,83 @@ func main() {
 			fmt.Println("Iteration", curr.String()+"/"+y.String(), "remaning time is", rem)
 		}
 		
-		x = ExpByPowOfTwoModular(x, expOneSecond, n)
+		exponent = mod(big.NewInt(0).Mul(exponent, expOneSecond), phi)
 
 		curr.Add(curr,one)
 
 	}
 
+	x = ExpByPowOfTwoModular(x, exponent, n)
+
 	fmt.Println("################")
 	fmt.Println("Time-Lock Puzzle finished !")
+	fmt.Println("")
+	fmt.Println("time to unlock:")
+	fmt.Println(time.Duration(int(y.Int64()))*time.Second)
+	fmt.Println("N:")
+	fmt.Println(n)
+	fmt.Println("base:")
+	fmt.Println(base)
+	fmt.Println("expOneSecond:")
+	fmt.Println(expOneSecond)
+	fmt.Println("iter:")
+	fmt.Println(iter)
+	fmt.Println("result:")
+	fmt.Println(x)
+	fmt.Println("sha256 of result:")
+	fmt.Println(sha256AndHex(x.Bytes()))
+	fmt.Println("")
+	fmt.Println("################")
+}
+
+func solvePuzzle() {
+	//we generate the parameters
+	n := readBig("Please enter n, the modulus :")
+	fmt.Println("n's hash is", sha256AndHex(n.Bytes()))
+	base := readBig("Please enter the base :")
+	fmt.Println("base's hash is", sha256AndHex(base.Bytes()))
+	iter := readBig("Please enter the number of iterations :")
+
+	x := base
+
+	curr := big.NewInt(0)
+	one := big.NewInt(1)
+	zero := big.NewInt(0)
+	hundred := big.NewInt(100)
+
+	for curr.Cmp(iter) < 0 {
+		if mod(curr, hundred).Cmp(zero) == 0 {
+			diff := big.NewInt(0).Sub(iter, curr)
+			rem := time.Duration(int(diff.Int64())) * time.Second
+			fmt.Println("Iteration", curr.String()+"/"+iter.String(), ", remaining time is", rem)
+		}
+		
+		x = ExpByPowOfTwoModular(x, expOneSecond, n)
+
+		curr.Add(curr,one)
+	}
+
+	fmt.Println("################")
+	fmt.Println("Time-Lock Puzzle solved !")
 	fmt.Println("")
 	fmt.Println(x)
 	fmt.Println("")
 	fmt.Println(sha256AndHex(x.Bytes()))
 	fmt.Println("")
 	fmt.Println("################")
-	fmt.Println("****************")
+}
 
+func readBig(text string) *big.Int {
+	reader := bufio.NewReader(os.Stdin)
 
+	fmt.Print(text)
+	s, _ := reader.ReadString('\n')
+	s = strings.Replace(s, "\n", "", 1)
 
+	y := big.NewInt(0)
+	y.SetString(s, 10)
+
+	return y
 }
 
 func sha256AndHex(x []byte) string {
